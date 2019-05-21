fb323e83cf0eb241ef2e54c92392bc99c5987c4c
tamjidtayeb <tayebtamjid@gmail.com>
2019-05-21 20:14:13 +1000

    added stock question

---
+++ src/stock.java
@@ -0,0 +1,22 @@
+public class stock {
+    static int solution(int[] prices) {
+        int maxProfit=0;
+        int min=Integer.MAX_VALUE;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) min = prices[i];
+            else {
+                //check how much profit if we sell now
+                maxProfit = Math.max(maxProfit,prices[i]-min);
+            }
+        }
+        return maxProfit;
+        // int profit=0;
+        // for (int i = 0; i<prices.length;i++) {
+        //     for (int j=i+1; j<prices.length;j++) {
+        //         if (prices[j]-prices[i] > profit) profit = prices[j]-prices[i];
+        //     }
+        // }
+        // return profit;
+    }
+}
